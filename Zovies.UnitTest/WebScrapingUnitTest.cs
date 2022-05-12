6365f06b368170d590e54f519c25d35d86c455c8
Zed <emailzed03@gmail.com>
2022-05-12 16:05:45 +1000

    cleaned up unit tests

--- Zovies.UnitTest/WebScrapingUnitTest.cs
+++ Zovies.UnitTest/WebScrapingUnitTest.cs
@@ -9,13 +9,23 @@ namespace Zovies.UnitTest;
 public class Tests
 {
     private WebAutomater automation;
+
+    private int CorrectYear;
+    private string CorrectName;
+    private string MovieUrl;
+        
+    
     
     [SetUp]
     public void Setup()
     {
-        automation = new WebAutomater("");
+        automation = new WebAutomater();
+        MovieUrl = "https://lookmovie2.to/movies/view/13320622-le-secret-de-la-cite-perdue-2022";
+        CorrectName = "The Lost City";
+        CorrectYear = 2022;
     }
 
+    // Basic test just to see if the webdriver is running correctly
     [Test]
     public void Test1()
     {
@@ -32,15 +42,26 @@ public class Tests
     [Test]
     public void TestFetchingMovieNameAndYear()
     {
-        var correctYear = 2022;
-        var correctName = "The Lost City";
-        automation.NavigateTo("https://lookmovie2.to/movies/view/13320622-le-secret-de-la-cite-perdue-2022");
+        
+        automation.NavigateTo(MovieUrl);
         var (year, name) = automation.GetDetails();
 
-        Assert.AreEqual(correctYear, year);
-        Assert.AreEqual(correctName, name);
+        Assert.AreEqual(CorrectYear, year);
+        Assert.AreEqual(CorrectName, name);
     }
-
+    
+    
+    [Test]
+    public void TestGettingM3u8File()
+    {
+        automation.NavigateTo(MovieUrl);
+        automation.OpenPlayer();
+        var file = automation.GetHdMu38FileUrl();
+        Console.WriteLine(file ?? "null :( no good resolution");
+    }
+    
+    
+    
     [TearDown]
     public void Teardown() {
         automation.Close();

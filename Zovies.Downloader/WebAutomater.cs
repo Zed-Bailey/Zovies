ba9b73a9f1cb08424f11c3c8b119505d36dd8eb7
Zed <emailzed03@gmail.com>
2022-05-12 16:04:53 +1000

    No longer need to watch selenium network requests
    Am now executing a javascript snippet that access the videojs player and the QualitySelector plugin
    it then gets the HD quality item and return the m3u8 file url!

--- Zovies.Downloader/WebAutomater.cs
+++ Zovies.Downloader/WebAutomater.cs
@@ -17,35 +17,75 @@ public class WebAutomater
      * 
      */
     private IWebDriver _driver;
-    private string _baseUrl;
+    // public List<string> m3u8FileUrl = new();
     
-    public WebAutomater(string baseUrl)
+    
+    public WebAutomater()
     {
         _driver = new ChromeDriver();
         // start monitoring network requests
-        _driver.Manage().Network.StartMonitoring().Wait();
+        // _driver.Manage().Network.StartMonitoring().Wait();
         // add event handler, triggered for every request sent
-        _driver.Manage().Network.NetworkRequestSent += LogNetworkRequest;
-        
-        // _driver.Url = baseUrl;
-        _baseUrl = baseUrl;
+        // _driver.Manage().Network.NetworkRequestSent += LogNetworkRequest;
     }
 
     public void NavigateTo(string url)
     {
         _driver.Url = url;
     }
+    
+    /// <summary>
+    /// Clicks on the video player play button, opening the video player
+    /// </summary>
     public void OpenPlayer()
     {
         var exists = _driver.FindElement(By.ClassName("round-button"));
-        exists?.Click();
+        if (exists != null)
+        {
+            exists.Click();
+            // sleep after click on the player as there may be a thread defence page
+            // between the movie detail and move player pages
+            Thread.Sleep(5000);
+        }
     }
 
-    public void PlayMovie()
+    /// <summary>
+    /// Executes a javascript script that will fetch the hd resolution mu38 file from the video players quality selector  
+    /// </summary>
+    /// <returns></returns>
+    public string? GetHdMu38FileUrl()
     {
-        var player = _driver.FindElement(By.Id("video_player_html5_api"));
-        player?.Click();
+        /*
+         * working javascript script that fetches the source from the quality inspector, also
+         * source.src is the m3u8 file, now we dont need to watch the network logs!
+         * created by playing around in the developer console
+         * and discovering that the player uses this plugin: https://github.com/silvermine/videojs-quality-selector
+         // creates a new videojs instance
+         var player = videojs('video_player_html5_api');
+         // get the items in the quality panel
+         var items = player.controlBar.getChild("QualitySelector").items;
+        // theres only 2 resolutions available for non-members 480p and HD
+        // HD is the second array element 
+        if(items.length == 2) {
+            // this will return the m3u8 file url for the HD video 
+              return items[1].source.src
+        }
+        
+        //if(items[1].source.label == 'HD') {}
+         */
+        var execute = _driver as IJavaScriptExecutor;
+        var response = execute?.ExecuteScript(@"
+            var player = videojs('video_player_html5_api');
+            var items = player.controlBar.getChild('QualitySelector').items;
+            if(items.length == 2) {
+                    return items[1].source.src
+            }
+        ");
+        return response as string;
     }
+    
+    
+    
 
     /// <summary>
     /// Get the movies release year and movies name from the page
@@ -59,13 +99,19 @@ public class WebAutomater
         return (int.Parse(year), name);
     }
     
-    private static void LogNetworkRequest(object? sender, NetworkRequestSentEventArgs args)
-    {
-        if(args.RequestUrl.EndsWith(".m3u8"))
-            Console.WriteLine($"{args.RequestMethod} | {args.RequestUrl}");
-    }
-    
+    // private void LogNetworkRequest(object? sender, NetworkRequestSentEventArgs args)
+    // {
+    //     if (!args.RequestUrl.EndsWith(".m3u8")) return;
+    //     
+    //     Console.WriteLine($"{args.RequestMethod} | {args.RequestUrl}");
+    //     m3u8FileUrl.Add(args.RequestUrl);
+    //
+    // }
+    //
 
+    /// <summary>
+    /// Close the browser
+    /// </summary>
     public void Close()
     {
         // stop the network monitoring

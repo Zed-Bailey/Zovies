9ff0da6e988ec3a8c7aa05bb86ec64a1df6a2b58
Zed <emailzed03@gmail.com>
2022-05-12 14:07:51 +1000

    implemeting the omdb service api calls,

--- Zovies.Backend/Services/OMDBService.cs
+++ Zovies.Backend/Services/OMDBService.cs
@@ -1,16 +1,44 @@
 using Zovies.Backend.Models;
-
+using System.Web;
 namespace Zovies.Backend.Services;
 
 public class OMDBService
 {
     private readonly string _apiKey;
+    private HttpClient _client;
     public OMDBService(string apiKey)
     {
         _apiKey = apiKey;
+        _client = new HttpClient();
     }
 
-    // public async Task<Movie> FetchMovieDetails(string movieName, int releaseYear)
-    // {
-    // }
+    /// <summary>
+    /// Query omdb api for movie information
+    /// </summary>
+    /// <param name="movieName"></param>
+    /// <param name="releaseYear"></param>
+    /// <returns></returns>
+    public async Task<Movie?> FetchMovieDetails(string movieName, int releaseYear)
+    {
+        var baseUrl = $"http://www.omdbapi.com/?apikey={_apiKey}&t={movieName}&y={releaseYear}";
+        var omdb = await _client.GetFromJsonAsync<OMDBModel>(HttpUtility.UrlEncode(baseUrl));
+        if (omdb != null || omdb?.Response == "True")
+        {
+            return new Movie
+            {
+                MovieName = omdb.Title,
+                MovieCast = omdb.Actors,
+                MovieDetails = new Details
+                {
+                    Description = omdb.Plot,
+                    Rating = float.Parse(omdb.ImdbRating),
+                    Year = int.Parse(omdb.Year),
+                    MovieGenres = omdb.Genre,
+                    MovieCoverPath = omdb.Poster
+                }
+            };
+        }
+
+        return null;
+    }
 }

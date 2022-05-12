e2385d6b1f16907aacf5a3b061d74dcb6f289a2c
Zed <emailzed03@gmail.com>
2022-05-12 16:05:58 +1000

    working on the class to execute the download script

---
+++ Zovies.Downloader/Download.cs
@@ -0,0 +1,30 @@
+using Zovies.Backend.Models;
+using Zovies.Backend.Services;
+
+namespace Zovies.Downloader;
+
+public class Download
+{
+    private string _url;
+    private WebAutomater _automation;
+    
+    public Download(string url)
+    {
+        _url = url;
+        _automation = new WebAutomater();
+
+    }
+
+    // public async Movie GetMovie()
+    // {
+    //     _automation.NavigateTo(_url);
+    //     Thread.Sleep(1000);
+    //     var (year, name) = _automation.GetDetails();
+    //     var service = new OMDBService(ApplicationData.OMDB_Key);
+    //     await service.FetchMovieDetails(name, year);
+    //
+    //     // command: 
+    //     //
+    //     
+    // }
+}

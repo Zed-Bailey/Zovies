9ff64e77c1013ff822943e6e61e22fff872357e6
Zed <emailzed03@gmail.com>
2022-05-13 17:04:27 +1000

    update models naming conventions

--- Zovies.Backend/Models/Details.cs
+++ Zovies.Backend/Models/Details.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Zovies.Backend.Models;
 
 public class Details
 {
-    public int DetailsID { get; set; }
+    // primary key
+    public int DetailsId { get; set; }
+    // foreign key
+    public int MovieId { get; set; }
     public int Year { get; set; }
     public string MovieGenres { get; set; }
     public float Rating { get; set; }
     public string Description { get; set; }
     public string MovieFilePath { get; set; }
     public string MovieCoverPath { get; set; }
+    
+    // relationship to movie
+    public Movie Movie { get; set; }
 }
--- Zovies.Backend/Models/Movies.cs
+++ Zovies.Backend/Models/Movies.cs
@@ -2,9 +2,12 @@ namespace Zovies.Backend.Models;
 
 public class Movie
 {
-    public int MovieID { get; set; }
+    // primary key
+    public int MovieId { get; set; }
     public string MovieName { get; set; }
     // Comma seperated string of movie cast members
     public string MovieCast { get; set; }
+    
+    // relationship to details 
     public Details MovieDetails { get; set; }
 }

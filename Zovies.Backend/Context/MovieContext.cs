56c2042b97d7efd677af527fba4437670886955b
Zed <emailzed03@gmail.com>
2022-05-13 18:37:03 +1000

    added functions to simplify fetching the movie data from the db

--- Zovies.Backend/Context/MovieContext.cs
+++ Zovies.Backend/Context/MovieContext.cs
@@ -15,12 +15,39 @@ public class MovieContext : DbContext
 
     public string DbPath { get; }
 
+    /*
+     * .Include() will load the Detail model from the databse
+     * https://docs.microsoft.com/en-us/aspnet/web-api/overview/data/using-web-api-with-entity-framework/part-4
+     * this can be removed by changing the models relationship to 'virtual' so it will be loaded automatically by EF
+     * 
+     */
+    public Movie? GetMovie(int withId)
+    {
+        
+        return Movies
+            .Include(d => d.MovieDetails)
+            .AsNoTracking()
+            .FirstOrDefault(x => x.MovieId == withId);
+    }
+
+    public async Task<IEnumerable<Movie>> GetAll()
+    {
+        return await Movies
+            .Include(m => m.MovieDetails)
+            .AsNoTracking()
+            .ToListAsync();
+    }
+    
+
+
     public MovieContext()
     {
         var folder = Environment.SpecialFolder.LocalApplicationData;
         var path = Environment.GetFolderPath(folder);
-        DbPath = System.IO.Path.Join(path, "movies.db");
+        DbPath = Path.Join(path, "movies.db");
     }
+    
+    
     
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite($"Data Source={DbPath}");

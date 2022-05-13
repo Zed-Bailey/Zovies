9de69eaeead79b0df2961c62cdaf47239275b610
Zed <emailzed03@gmail.com>
2022-05-13 17:04:56 +1000

    added try catch in case of empty settings fields

--- Zovies.Backend/Program.cs
+++ Zovies.Backend/Program.cs
@@ -10,14 +10,22 @@ IConfiguration configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
 // configure application data class
-// this should be the url of the save folder on your network
-// eg. http://192.1.x.x/save/movie/folder/
-ApplicationData.NetworkDriveUrl = configuration["NetworkDriveUrl"];
-// and this should be the same folder path as what's in the network drive url
-// eg. /save/movie/folder
+try
+{
+    // the network url of the folder where you want to download and access the movies from on this device
+    // eg. http://192.1.x.x/save/movie/folder/
+    ApplicationData.NetworkDriveUrl = configuration["NetworkDriveUrl"];
+    // extract the save folder path from the network url
+    ApplicationData.SaveFolderPath = new Uri(ApplicationData.NetworkDriveUrl).AbsolutePath.TrimEnd('/');
+    ApplicationData.OmdbKey = configuration["OMDBApiKey"];
+}
+catch (Exception e)
+{
+    Console.WriteLine("ERROR:\nSeems like you did not fill in the appsettings.json file.\nMake sure to fill in the api key and network drive fields!");
+    return;
+}
 
-ApplicationData.SaveFolderPath = new Uri(ApplicationData.NetworkDriveUrl).AbsolutePath;
-ApplicationData.OmdbKey = configuration["OMDBApiKey"];
+
 
 
 var builder = WebApplication.CreateBuilder(args);

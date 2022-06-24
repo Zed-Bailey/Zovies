a6cf5e118fd1915571d176d00598bbb50bf22c90
Zed <emailzed03@gmail.com>
2022-06-24 14:50:22 +1000

    remove model, not used

--- Zovies.Backend/Controllers/FilterParams.cs
+++
@@ -1,8 +0,0 @@
-namespace Zovies.Backend.Controllers;
-
-public class FilterParams
-{
-    public string? Genre { get; } = null;
-    public float? Rating { get; } = null;
-    public string? SearchTerm { get; } = null;
-}
